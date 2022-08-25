7f45c7a243cc818a7fb9dafd9e551e1f0d0fa77f
DiegoEscCab <diego.esc.cab@gmail.com>
2022-08-25 00:06:39 -0500

    Arraylist 4/5 operations

--- Practices/src/main/java/ArrayList.java
+++ Practices/src/main/java/ArrayList.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 public class ArrayList {
 
     public static void main(String[] args) {
-        String  operator, language = "", languageType = "";
-        Integer index;
+        String  language = "", languageType = "",type = "";
+        Integer index, operator;
         Integer options = 0;
 
         Scanner scannerLanguage = new Scanner(System.in);
@@ -15,33 +15,55 @@ public class ArrayList {
         Scanner scannerOperation = new Scanner(System.in);
         Scanner scannerOptions = new Scanner(System.in);
         Scanner scannerIndex = new Scanner(System.in);
+        Scanner scannerLetter = new Scanner(System.in);
         List<LanguagePrograms> DB = DataUtils.loadList();
-        System.out.println("What you add or remove?");
-        operator = scannerOperation.next();
+        System.out.println("Press 0 and Enter to start");
+        operator = scannerOperation.nextInt();
 
-        System.out.println(operator);
 
         while (options.equals(0)) {
-            if (operator.equals("add")) {
+            System.out.println("Choose from this list: \n1 = Add\n2 = Remove Index\n3 = Remove Language\n4 = Remove Type\n5 = Remove Letter");
+            operator = scannerOperation.nextInt();
+            if (operator.equals(1)) {
                 System.out.println("Define the name of the language: ");
                 language = scannerLanguage.next();
                 System.out.println("Define the type of the language: ");
                 languageType = scannerLanguageType.next();
-
                 LanguagePrograms newLan = new LanguagePrograms(language, languageType);
                 DB.add(newLan);
-
                 DB.stream().forEach(System.out::println);
-            } else if (operator.equals("remove")){
 
-            }else if  {
+                //Remove by index
+            } else if (operator.equals(2 )){
                 System.out.println("What number of the list do you want remove?");
                 index = scannerIndex.nextInt();
-
-                System.out.println(index);
-
                 DB.remove(index.intValue());
                 DB.stream().forEach(System.out::println);
+
+                //Remove by Language
+            } else if (operator.equals(3)) {
+                System.out.println("Define the name of language to remove: ");
+                language = scannerLanguage.next();
+                String finalLanguage = language;
+                DB.stream().filter(LanguagePrograms -> !LanguagePrograms.getLanguage().equalsIgnoreCase(finalLanguage))
+                        .forEach(System.out::println);
+
+                //Remove by Type
+            } else if (operator.equals(4)) {
+                System.out.println("What  of the list do you want remove?");
+                System.out.println("Define the type to remove language: ");
+                languageType = scannerLanguageType.next();
+                String finalLanguageType = languageType;
+                DB.stream().filter(LanguagePrograms -> !LanguagePrograms.getType().equalsIgnoreCase(finalLanguageType))
+                        .forEach(System.out::println);
+
+                //Remove by Letter
+            } else if (operator.equals(5)) {
+                System.out.println("What  of the list do you want remove?");
+                System.out.println("Define the letter to remove language: ");
+                type = scannerLetter.next();
+                String finalType = type;
+                ///Falta realizar esta [ultima operacion
 
             } else {
                 System.out.println("no operation selected");
--- Practices/src/main/java/DataUtils.java
+++ Practices/src/main/java/DataUtils.java
@@ -9,13 +9,8 @@ public class DataUtils {
                 LanguagePrograms n03 = new LanguagePrograms("Java", "POO");
                 LanguagePrograms n04 = new LanguagePrograms("Kotlin", "Scripting");
                 LanguagePrograms n05 = new LanguagePrograms("Python", "Multi-paradigm");
-                LanguagePrograms n06 = new LanguagePrograms("Ruby", "Dynamic");
-                LanguagePrograms n07 = new LanguagePrograms("JavaScript", "Scripting");
-                LanguagePrograms n08 = new LanguagePrograms("PHP", "Scripting");
-                LanguagePrograms n09 = new LanguagePrograms("Perl", "Scripting");
-                LanguagePrograms n10 = new LanguagePrograms("Pascal", "Imperative");
-                LanguagePrograms n11 = new LanguagePrograms("SQL", "Declarative");
-                LanguagePrograms n12 = new LanguagePrograms("Go", "Concurrent");
+                LanguagePrograms n06 = new LanguagePrograms("Perl", "Scripting");
+                LanguagePrograms n07 = new LanguagePrograms("Ruby", "Dynamic");
 
                 list.add(n01);
                 list.add(n02);
@@ -24,12 +19,6 @@ public class DataUtils {
                 list.add(n05);
                 list.add(n06);
                 list.add(n07);
-                list.add(n08);
-                list.add(n09);
-                list.add(n10);
-                list.add(n11);
-                list.add(n12);
-
 
         return (list);
     }
