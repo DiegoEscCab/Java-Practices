72b5298436542f72acd4e83c6ee06b358a74044c
DiegoEscCab <diego.esc.cab@gmail.com>
2022-08-30 19:40:55 -0500

    Reformat code MyHashMap

--- Practices/src/main/java/MyHashMap.java
+++ Practices/src/main/java/MyHashMap.java
@@ -1,8 +1,3 @@
-// Create a HashMap class base collection class that is used to store key-value pairs.
-// Follow the below steps to create a HashMap class.
-// Map is empty
-// Size of map is -3
-
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -11,7 +6,6 @@ public class MyHashMap {
         Scanner scanner = new Scanner(System.in);
         HashMap<String, String> map = new HashMap<>();
 
-        // Show me the hashmap empty
         System.out.println("Map is empty");
         System.out.println(map);
 
@@ -24,20 +18,16 @@ public class MyHashMap {
             map.put(key, value);
         }
 
-        // Show me the size of the map
         System.out.println("Size of map is: " + map.size());
 
-        // Show me the hashmap with elements like an object
         System.out.println("Map with elements");
         System.out.println(map);
 
-        // Show me the key-value of the first item
         System.out.println("Key-value of the first item");
         System.out.println(map.keySet().iterator().next() + ": " + map.get(map.keySet().iterator().next()));
 
-        // Delete all elements from the map and show me the map empty
         map.clear();
         System.out.println("Map is empty");
         System.out.println(map);
-        }
     }
+}
